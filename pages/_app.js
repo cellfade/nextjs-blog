da78beefe8a1d612704082b892efe15b1d42d56c
cellfade <44656818+cellfade@users.noreply.github.com>
2023-04-16 15:35:16 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 // `pages/_app.js`
 import 'tailwindcss/tailwind.css'
-
+import '../styles/global.css';
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({Component, pageProps}) {
     return <>
+            <ThemeProvider attribute="class" enableSystem={false}>
             <Navigation/>
             <Component {...pageProps} />
             <Footer/>
+            </ThemeProvider>
     </>
 }
 
