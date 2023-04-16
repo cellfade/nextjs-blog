5f2b6cfb38af6b7df8b5f0280d9bc5853c675106
cellfade <44656818+cellfade@users.noreply.github.com>
2023-04-16 14:44:19 -0600

    Update _app.js
    removed "import '../styles/global.css';"

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // `pages/_app.js`
 import 'tailwindcss/tailwind.css'
-import '../styles/global.css';
+
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
