1f863ef5c7e0302fb00149227b191eb1d7c5f773
cellfade <44656818+cellfade@users.noreply.github.com>
2023-04-16 15:47:21 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,6 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-<About/>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
