4d3bbc386dfbf76dafda08df24a9a5eb32625fbb
cellfade <44656818+cellfade@users.noreply.github.com>
2023-04-16 16:57:09 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,9 @@ export default function Home({ allPostsData }) {
       </section>
 <br />
       <section className={utilStyles.headingSm}>
-        <p>My name is cellfade! I am a product designer by day and developer/digital artist by night. I have 7+ years experience in product and ux/ui design, but have recently focused on service design at enterprise-scale. Classically trained and curious about new technologies, I am constantly exploring the unfolding digital environment around us.</p>
+        <p>My name is cellfade! I am a product designer by day and developer/digital artist by night. I have 7+ years experience in product and ux/ui design, but have recently focused on service design at enterprise-scale.</p>
+        <br />
+         <p>Classically trained and curious about new technologies, I am constantly exploring the unfolding digital environment around us.</p>
         <br />
         <p>Find me on <a href="https://twitter.com/cellfade">Twitter</a>!</p>
       </section>
