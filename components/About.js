dc49305f69e91a095db963e8aa814cabb48cad5b
cellfade <44656818+cellfade@users.noreply.github.com>
2023-08-28 19:02:06 -0600

    Update About.js

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ import React from "react"
 import Image from "next/image"
 import profile from "../public/profile.jpg"
 import appimage1 from "../public/appimage1.jpg"
-import appimage2 from "../public/appimage4.jpg"
+import appimage4 from "../public/appimage4.jpg"
 import appimage3 from "../public/appimage3.jpg"
 
 const About = () => {
